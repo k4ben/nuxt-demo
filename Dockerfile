770caaac044e4ff8ab948947466381a85af294c5
OutboundSpade <44787842+OutboundSpade@users.noreply.github.com>
2023-03-07 13:28:12 -0500

    Added ability to set mongodb uri through environment variables.
    if MONGO_URI is set, it will be used instead of DEFAULT_URI.

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ COPY . .
 
 RUN yarn build
 
+ENV MONGO_URI=mongodb://mongodb
+
 CMD node .output/server/index.mjs
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
+const DEFAULT_URI =
+	"mongodb+srv://uug:JAice8BJgqXo6DN4@uugdemo.pmtwnlx.mongodb.net/?retryWrites=true&w=majority";
 export default async () => {
-  try {
-    await mongoose.connect("mongodb+srv://uug:JAice8BJgqXo6DN4@uugdemo.pmtwnlx.mongodb.net/?retryWrites=true&w=majority");
-    console.log("DB connection established.");
-  } catch (err) {
-    console.error("DB connection failed.", err);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_URI ?? DEFAULT_URI);
+		console.log("DB connection established.");
+	} catch (err) {
+		console.error("DB connection failed.", err);
+	}
 };
